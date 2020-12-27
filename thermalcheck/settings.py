a4adae71a5a89f650de9ebe1ec305c33b55b8325
Youseop <66366941+youseop@users.noreply.github.com>
2020-12-27 14:56:45 +0900

    Update settings.py

--- thermalcheck/settings.py
+++ thermalcheck/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bxx@2-g6%df%z-^pdgo^#r-sr($6f%j&+u0%xx32z&&-s#&w2z'
+SECRET_KEY = <secret>
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
